94a207c21a1cd2f3002e63726deeb88fe1501a3d
Arlei Domingos <136842200+Arleisousa@users.noreply.github.com>
2023-07-24 22:08:31 -0300

    aprendendo variaveis em js

---
+++ javascript/variaveis/script.js
@@ -0,0 +1,25 @@
+let nome = "Arlei"
+let idade = 17
+let comidaFavorita
+comidaFavorita = 'Bolo'
+
+let var1, var2, var3, var4, var5
+
+console.log (nome, idade, comidaFavorita, var1, var2, var3, var4, var5)
+
+
+
+
+
+
+// Declarar uma variável com o seu nome
+
+// Declarar uma variável com a sua idade
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+// Atribuir valor a sua comida favorita
+
+// Declarar 5 variáveis diferentes sem valores
+
