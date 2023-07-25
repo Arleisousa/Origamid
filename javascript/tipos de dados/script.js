5854f78a8c226b4f9fed9764484adab54c1943c9
Arlei Domingos <136842200+Arleisousa@users.noreply.github.com>
2023-07-25 01:00:29 -0300

    java script

--- javascript/tipos de dados/script.js
+++ javascript/tipos de dados/script.js
@@ -1,3 +1,32 @@
-let nome = true
+let fruta = 'Banana' 
+let num = '14'
+let idade = 17
 
+let nome = 'Arlei '
+let sobrenome = 'Domingos'
+let nomeCompleto = nome + sobrenome
+
+let frase = "it's Time"
+
+
+
+console.log (nome)
+console.log (typeof num)
+console.log (idade)
+console.log (nomeCompleto)
+console.log (frase)
 console.log (typeof nome)
+
+
+// Declare uma variável contendo uma string
+
+// Declare uma variável contendo um número dentro de uma string
+
+// Declare uma variável com a sua idade
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+// Coloque a seguinte frase em uma variável: It's time
+
+// Verifique o tipo da variável que contém o seu nome
