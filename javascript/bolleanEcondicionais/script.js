e3a5e1006ea936c3f03aeca35896e41cfcf16ed4
Arlei Domingos <136842200+Arleisousa@users.noreply.github.com>
2023-08-04 03:25:55 -0300

    aprendendo javascript

---
+++ javascript/bolleanEcondicionais/script.js
@@ -0,0 +1,38 @@
+let possuiGrad = false
+let possuiDot = false
+let possuiDoteGrad = false
+let possuiNada = true
+
+if (possuiGrad) {
+    console.log('Ele tem grad')
+} else if(possuiDot) {
+    console.log('Ele não tem grad mas tem doutorado')
+} else if (possuiDoteGrad) {
+    console.log ('Ele possui dot e grad')
+} else {
+    console.log('Ele não tem nada morando em barraco')
+}
+
+let nome = ''
+
+if(nome) {
+    console.log('Esse é meu nome')
+} else {
+    console.log('Seu nome não foi definido')
+}
+
+let possuiCarro = true
+
+if (!possuiCarro) {
+    console.log('Isso é verdadeiro')
+} else {
+    console.log('isso não é verdadeiro')
+}
+
+let number = 15
+
+if (number > 3) {
+    console.log ('verdadeiro')
+} else {
+    console.log ('falso')
+}
