2ce759052a0191e64bd2ad8ce0aa7163dcdb8ef0
Arlei Domingos <136842200+Arleisousa@users.noreply.github.com>
2023-08-04 23:31:49 -0300

    estudando javascript

--- javascript/bolleanEcondicionais/script.js
+++ javascript/bolleanEcondicionais/script.js
@@ -36,3 +36,71 @@ if (number > 3) {
 } else {
     console.log ('falso')
 }
+
+let corFav = 'cinza'
+
+switch (corFav) {
+    case 'Verde':
+        console.log ('Essa é minha cor favorita')
+        break
+    case 'rosa':
+        console.log('Eu gosto dessa cor da barbie')
+        break
+    case 'vermelho':
+        console.log('eu gosto dessa cor de sangue')
+        break
+    case 'azul':
+        console.log('eu gosto dessa cor do céu')
+        break
+        default:
+        console.log('minha cor favorita não foi encontrada')
+}
+
+// Verifique se a sua idade é maior do que a de algum parente
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+
+let minhaIdade = 17
+let idadeParente = 13
+
+if (minhaIdade > idadeParente) {
+    console.log('Minha idade é maior')
+} else if (minhaIdade === idadeParente) {
+    console.log('É igual')
+} else (
+    console.log('Minha idade é menor')  
+)
+
+
+// Qual valor é retornado na seguinte expressão?
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
+
+// Verifique se as seguintes variáveis são Truthy ou Falsy
+var name = 'Andre'; //truthy
+var idade = 28; //truyhy
+var possuiDoutorado = false; //false
+var empregoFuturo; //false
+var dinheiroNaConta = 0; //false
+
+// Compare o total de habitantes do Brasil com China (valor em milhões)
+var brasil = 207;
+var china = 1340;
+
+if (brasil > china) {
+    console.log ('Brasil é maior que a china')
+} else {
+    console.log('Brasil é menor que a china')
+}
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') && (5 > 2)) {
+  console.log('Verdadeiro');
+} else {
+  console.log('Falso');
+}
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') || (5 > 2)) {
+  console.log('Gato' && 'Cão');
+} else {
+  console.log('Falso');
+}
