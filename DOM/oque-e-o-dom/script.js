aeae4bb2364ff9ea642361b405faa28b0b80717d
Arlei Domingos <136842200+Arleisousa@users.noreply.github.com>
2023-08-21 01:50:07 -0300

    DOM

---
+++ DOM/oque-e-o-dom/script.js
@@ -0,0 +1,27 @@
+const h1Selecionado = document.querySelector('h1')
+
+console.log(h1Selecionado)
+
+// Retorne o url da página atual utilizando o objeto window
+const urlPag = window.location.href 
+
+console.log(urlPag)
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+const elementoAtv = document.querySelector('.ativo')
+
+console.log(elementoAtv)
+
+// Retorne a linguagem do navegador
+
+const linguagemNav = window.navigator.language
+console.log(linguagemNav)
+
+// Retorne a largura da janela 
+
+
+const tamanhoJan = window.screen.availWidth
+
+console.log(tamanhoJan)
